6dccd094d6e52238d17c36d0ccb6da65a65946cb
ChadMWatts <chadmwatts@gmail.com>
2016-05-12 14:31:12 -0600

    journal can save

--- Journal/Entry.swift
+++ Journal/Entry.swift
@@ -9,12 +9,38 @@
 import Foundation
 
 class Entry: Equatable {
+    
+    private let kTimeStamp = "timeStamp"
+    private let kTitle = "title"
+    private let kBodyText = "bodyText"
+    
     var timeStamp: NSDate
     var title: String
     var bodyText: String
     
+    var dictionaryCopy: [String:AnyObject] {
+        return [kTimeStamp: timeStamp, kTitle: title, kBodyText: bodyText]
+    }
+    
     init(timeStamp: NSDate = NSDate(), title: String, bodyText: String) {
         self.timeStamp = NSDate()
+        self.title = title
+        self.bodyText = bodyText
+    }
+    
+    init?(dictionary: [String:AnyObject]) {
+        guard let timeStamp = dictionary[kTimeStamp] as? NSDate,
+        title = dictionary[kTitle] as? String,
+            bodyText = dictionary[kBodyText] as? String else {
+                
+                self.timeStamp = NSDate()
+                self.title = ""
+                self.bodyText = ""
+                
+                return nil
+        }
+        
+        self.timeStamp = timeStamp
         self.title = title
         self.bodyText = bodyText
     }
